fc1cc99a7782fbcb3e046d990e08b6703d2f8ea4
Simon D. Levy <simon.d.levy@gmail.com>
2026-04-07 16:02:54 -0400

    =More ekfgen

--- ekfgen.py
+++ ekfgen.py
@@ -13,9 +13,14 @@ def main():
 
     parser = argparse.ArgumentParser(
             formatter_class=ArgumentDefaultsHelpFormatter)
-
     parser.add_argument('n', help='Stae vector dimensionality')
-
     args = parser.parse_args()
 
+    n = int(args.n)
+
+    print('#pragma once\n\n')
+    print('class EkfCore {\n\n')
+    print('    public:\n\n')
+
+ 
 main()
